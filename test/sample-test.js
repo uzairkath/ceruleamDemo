c66ccc22788bd53bbb2b5cf31432a99ac206c250
uzairkath <uzairkath@gmail.com>
2022-05-21 22:52:02 +0500

    remove console.log statements from sample-test.js

--- test/sample-test.js
+++ test/sample-test.js
@@ -20,7 +20,6 @@ describe("nfts", function () {
    });
   it("Should mint the token of first contract", async function () {
     const balanceBefore = await firstNFT.balanceOf(owner.address);
-    console.log(balanceBefore);
     await firstNFT.mint(owner.address, {from: owner.address, value: ethers.utils.parseEther("0.1")});
     const balanceAfter = await firstNFT.balanceOf(owner.address);
     // to check the balance after the token minted
@@ -29,43 +28,33 @@ describe("nfts", function () {
     await expect(firstNFT.mint(owner.address, {from: owner.address, value: ethers.utils.parseEther("0.1")}))
   .to.emit(firstNFT, 'Transfer')
   .withArgs(constants.ZERO_ADDRESS, owner.address, 1);
-   console.log(await firstNFT.balanceOf(owner.address)); 
   });
   
   it("Should mint the token of second contract in taking first token", async function () {
     const balanceBefore = await secondNFT.balanceOf(owner.address);
-    console.log('beforeBalance nft2 is ', balanceBefore)
     await firstNFT.mint(owner.address , {value: ethers.utils.parseEther("0.1")});
     const balanceBefore1 = await firstNFT.balanceOf(owner.address);
-    console.log('beforeBalance nft1 is ', balanceBefore1)
     // approving first so that transferFrom can be used
     await firstNFT.approve(secondNFT.address, 1, {from: owner.address});
     await secondNFT.exchange(1);
     const balanceAfter = await secondNFT.balanceOf(owner.address);
-    console.log('beforeAfter nft2 is ', balanceAfter);
     const contractBalance = await firstNFT.balanceOf(secondNFT.address);
     const balanceAfter1 = await firstNFT.balanceOf(owner.address);
-    console.log('beforeAfter nft1 is ', balanceAfter1);
     // to check the balance after token minted
     expect(balanceAfter -1).to.equal(balanceBefore);
     // to check the contract balance after exchange
     expect(contractBalance).to.equal(1);
     // to check the balance after token of firstNFT contract exchanged
     expect(parseInt(balanceAfter1.toString()) + 1).to.equal(balanceBefore1);
-    console.log(await secondNFT.ownerOf(0));
 
   })
 
     it("Should swap the token of second contract with the token of first contract", async function () {
       const balanceBefore = await secondNFT.balanceOf(owner.address);
-      console.log('beforeBalance nft2 is ', balanceBefore)
       const balanceBefore1 = await firstNFT.balanceOf(owner.address);
-      console.log('beforeBalance nft1 is ', balanceBefore1)
       await secondNFT.swap(0, {from: owner.address});
       const balanceAfter = await secondNFT.balanceOf(owner.address);
-      console.log('balanceAfter nft2 is ', balanceAfter);
       const balanceAfter1 = await firstNFT.balanceOf(owner.address);
-      console.log('balanceAfter nft1 is ', balanceAfter1);
       const contractBalance = await secondNFT.balanceOf(secondNFT.address);
       // to check the balance after tokens swapped
       expect(parseInt(balanceAfter.toString()) +1).to.equal(balanceBefore);
